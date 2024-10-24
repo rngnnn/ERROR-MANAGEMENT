4112f937c65dae52080c9ade69052a3d18f369cf
rngnnn <rngnnnclk@gmail.com>
2024-10-24 09:58:56 +0200

    PORT/HOST

--- error.js
+++ error.js
@@ -1,21 +1,43 @@
-const express= require('express')
-const app=express()
+"use strict"
+/* ------------------------------------------ 
+            Error Handling in NodeJS    
+------------------------------------------ */
 
-require('dotnv').config()
-const PORT=process.env.PORT||8000
+/* 
+* npm init -y
+* npm install express dotenv
+* echo PORT=8000 > .env
+* cat > .gitignore (kopyalanan verileri terminle yapistir ardindan ctrl + c ile cikis yap)
+*/
 
-app.get('/user/:id',(req,res)=>{
-res.send({
-   
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+app.get('/user/:id', (req, res) => {
+
+    const { id } = req.params
+
+    if (isNaN(id)) {
+            throw new Error('ID is not a number', { cause: "params.id"})
+    } else {
+        res.send({
+            error: false,
+            message: 'hi there',
+            id
+        })
+    }
+
 })
 
-})
 
 
 
 
 
 
+/*------------------------------------------ */
+app.listen(PORT, () => console.log(`Running at : http://127.0.0.1:${PORT} `))
 
-
-app.listen(PORT,()=>console.log(`Running at:http://127.0.0.1:${PORT}`))
